473602d26350165ada0898ee408e8edeabc6f47c
Scott Ross <sr523@cornell.edu>
2016-04-05 14:39:00 -0400

    added opt parse

--- main.rb
+++ main.rb
@@ -1,8 +1,23 @@
 require 'aws-sdk'
 require 'json'
+require 'optparse'
+
+
+# -r = region, -t name:value, -op operation (start/stop)
+OPTIONS = {}
+
+#Default Values
+
+OptionParser.new do |opt|
+  opt.on('--elb_name elb name') { |o| OPTIONS[:elb_name] = o }
+  opt.on('--app app name') { |o| OPTIONS[:app_name] = o }
+end.parse!
+
+bucket_name = OPTIONS[:elb_name] + "-logging"
+resource_name = "arn:aws:s3:::" + bucket_name +  "/" + OPTIONS[:app_name] + "/*"
 
 s3 = Aws::S3::Client.new(region: 'us-east-1')
-new_bucket = s3.create_bucket(bucket: 'team1-elb-logging-s3')
+new_bucket = s3.create_bucket(bucket: bucket_name)
 
 policy_json = {
   "Id" => "Policy1459871218139",
@@ -12,7 +27,7 @@ policy_json = {
       "Sid" => "Stmt1459871212751",
       "Action" => ["s3:PutObject"],
       "Effect" => "Allow",
-      "Resource" => "arn:aws:s3:::team1-elb-logging-s3/my-app/*",
+      "Resource" => resource_name,
       "Principal" => {
         "AWS" => [
           "127311923021"
@@ -23,7 +38,7 @@ policy_json = {
 }
 
 s3.put_bucket_policy({
-  bucket: "team1-elb-logging-s3",
+  bucket: bucket_name,
   policy: policy_json.to_json
 })
 
@@ -31,15 +46,15 @@ elb_client = Aws::ElasticLoadBalancing::Client.new(
   region: 'us-east-1'
 )
 resp = elb_client.modify_load_balancer_attributes({
-  load_balancer_name: "team1-elb", # required
-  load_balancer_attributes: { 
+  load_balancer_name: OPTIONS[:elb_name], # required
+  load_balancer_attributes: {
     access_log: {
       enabled: true, # required
-      s3_bucket_name: "team1-elb-logging-s3",
+      s3_bucket_name: bucket_name,
       emit_interval: 5,
-      s3_bucket_prefix: "my-app",
+      s3_bucket_prefix: OPTIONS[:app_name],
     }
   },
 })
 
-puts "Number of objects "+s3.list_objects(bucket:"team1-elb-logging-s3").contents.length.to_s
+puts "Number of objects "+s3.list_objects(bucket: bucket_name).contents.length.to_s
